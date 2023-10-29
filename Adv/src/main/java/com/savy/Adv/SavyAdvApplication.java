0608741f72121e00ed1eb9f36df7470d7afbd610
Sara <saraharris2000@gmail.com>
2023-10-28 18:20:38 -0700

    initialize spring

---
+++ Adv/src/main/java/com/savy/Adv/SavyAdvApplication.java
@@ -0,0 +1,13 @@
+package com.savy.Adv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavyAdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavyAdvApplication.class, args);
+	}
+
+}
---
+++ Adv/src/test/java/com/savy/Adv/SavyAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savy.Adv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavyAdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
