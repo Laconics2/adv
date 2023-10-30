5b838b307f789aa9f86a39f2b7657c9626c638fe
Sara <saraharris2000@gmail.com>
2023-10-29 20:41:25 -0700

    update to remove automatic configuration

--- Adv/src/main/java/com/savy/Adv/SavyAdvApplication.java
+++ Adv/src/main/java/com/savy/Adv/SavyAdvApplication.java
@@ -2,8 +2,9 @@ package com.savy.Adv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SavyAdvApplication {
 
 	public static void main(String[] args) {
