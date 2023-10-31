935bd3f52b40747478cbce659c10a32c750db650
Sara <saraharris2000@gmail.com>
2023-10-30 21:03:52 -0700

    add client service to serve as a way to interact with repo

--- Adv/src/main/java/com/savy/Adv/repositories/ClientRepository.java
+++ Adv/src/main/java/com/savy/Adv/repositories/ClientRepository.java
@@ -1,12 +1,9 @@
 package com.savy.Adv.repositories;
 
 import com.savy.Adv.dto.Client;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends CrudRepository<Client, Long> {
 }
