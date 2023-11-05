897a17b2e3e34d844a26a796884375ee1091f8ae
Sara <saraharris2000@gmail.com>
2023-11-04 22:42:13 -0700

    begin mongo configuration

--- adv/src/main/java/com/savy/adv/AdvApplication.java
+++ adv/src/main/java/com/savy/adv/AdvApplication.java
@@ -1,10 +1,16 @@
 package com.savy.adv;
 
+import com.savy.adv.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AdvApplication {
+	@Autowired
+	PlayerRepository playerCharacterRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvApplication.class, args);
