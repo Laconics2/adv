aeb14e17699ebfca6cbca5667ed8902a807cc292
Sara <saraharris2000@gmail.com>
2023-11-05 21:13:46 -0700

    player character inherit from entity, remove repetetiveness

--- adv/src/main/java/com/savy/adv/dto/Player.java
+++ adv/src/main/java/com/savy/adv/dto/Player.java
@@ -5,26 +5,14 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
 @Document("playercharacters")
-public class Player {
-    @Id
-    @Setter @Getter
-    private String id;
-
-    @Getter @Setter
-    private String name;
-    @Getter @Setter
-    private int healthPoints;
-    @Getter @Setter
-    private int powerLevel;
-    @Getter @Setter
+public class Player extends Entity{
+    @Setter
     private String playerClass;
 
     public Player(String id, String name, int healthPoints, int powerLevel, String playerClass){
-        super();
-        this.id = id;
-        this.name = name;
-        this.healthPoints = healthPoints;
+        super(id, name, healthPoints, powerLevel);
         this.playerClass = playerClass;
     }
 }
